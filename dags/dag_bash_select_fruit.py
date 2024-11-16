520fa1b496a446f566b1da4d77bb3a679d6e9fc6
leekm12 <uiuiui12@naver.com>
2024-11-17 04:29:47 +0900

    bash operator test1

--- dags/dag_bash_select_fruit.py
+++ dags/dag_bash_select_fruit.py
@@ -22,4 +22,4 @@ with DAG(
         bash_command="/opt/airflow/plugins/shell/select_fruit.sh AVOCADO",
     )
 
-    t0_grant_permission >> [t1_orange >> t2_avocado]
+    t0_grant_permission >> [t1_orange, t2_avocado]
